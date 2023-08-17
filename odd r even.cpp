7240d8d7a0681bdea8b253e404a76d3a26333ee5
madhankumar07 <122252714+madhankumar07@users.noreply.github.com>
2023-08-17 08:18:49 +0530

    Add files via upload

---
+++ odd r even.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int num[10],i;
+	printf("enter the 10 numbers:");
+	for(i=0;i<0;i++)
+	scanf("%d",&num[i]);
+	printf("\n even numbers are:\n");
+	for(i=0;i<0;i++)
+	{
+		if(num[i]%2==0);
+		printf("%d",num[i]);
+	}
+	printf("\n odd numbers are:\n");
+	for(i=0;i<0;i++)
+	{
+		if(num[i]%2!=0);
+		printf("%d",num[i]);
+	}
+	return 0;
+ } 
